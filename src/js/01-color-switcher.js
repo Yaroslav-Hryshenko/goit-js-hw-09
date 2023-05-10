63426c7ed017a8528d345596bbcad6241b4ada9e
Yaroslav-Hryshenko <yaroslav.cook81@gmail.com>
2023-05-10 10:15:24 +0300

    hw9\1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+np
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+function colorBody() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
 
+refs.btnStop.setAttribute('disabled', 'disabled');
+
+let color = null;
+
+refs.btnStop.addEventListener('click', () => {
+  clearInterval(color);
+
+  refs.btnStart.removeAttribute('disabled');
+  refs.btnStop.setAttribute('disabled', 'disabled');
+});
+
+refs.btnStart.addEventListener('click', () => {
+  clearInterval(color);
+  color = setInterval(colorBody, 1000);
+
+  refs.btnStop.removeAttribute('disabled');
+  refs.btnStart.setAttribute('disabled', 'disabled');
+});
